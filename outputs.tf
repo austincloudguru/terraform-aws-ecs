337af29836e06bb11c788dff7d2ca4a18206f892
Mark Honomichl <mark@austincloud.guru>
2019-11-07 10:15:15 -0600

    Added outputs

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------
+# Output
+#------------------------------------------------------------------------------
+output "cluster_id" {
+  value = aws_ecs_cluster.ecs_cluster.id
+}
+
+output "cluster_arn" {
+  value = aws_ecs_cluster.ecs_cluster.arn
+}
