6aefb22751969d0d73a144a2e0f34642548e806c
Mark Honomichl <mark@austincloud.guru>
2019-11-07 09:50:52 -0600

    Added Tags

--- variables.tf
+++ variables.tf
@@ -43,3 +43,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
