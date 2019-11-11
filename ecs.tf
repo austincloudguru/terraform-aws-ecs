a7023d0e03497c826c4765956aafd27bd5caad3d
Mark Honomichl <mark@austincloud.guru>
2019-11-11 11:11:54 -0600

    Changed Template to inline

--- ecs.tf
+++ ecs.tf
@@ -91,7 +91,34 @@ resource "aws_launch_configuration" "ecs_lc" {
 }
 
 data "template_file" "user_data" {
-  template = file("${path.module}/templates/user_data.tpl")
+  template = <<EOF
+Content-Type: multipart/mixed; boundary="==BOUNDARY=="
+MIME-Version: 1.0
+
+--==BOUNDARY==
+Content-Type: text/cloud-boothook; charset="us-ascii"
+
+# Install amazon-efs-utils
+cloud-init-per once yum_update yum update -y
+cloud-init-per once install_amazon-efs-utils yum install -y amazon-efs-utils
+
+# Create /efs folder
+cloud-init-per once mkdir_efs mkdir /efs
+
+# Mount /efs
+cloud-init-per once mount_efs echo -e '$${efs_id}:/ /efs efs defaults,_netdev 0 0' >> /etc/fstab
+mount -a
+
+--==BOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+
+#!/bin/bash
+# Set any ECS agent configuration options
+echo "ECS_CLUSTER=$${ecs_cluster_name}" >> /etc/ecs/ecs.config
+
+--==BOUNDARY==--
+
+EOF
 
   vars = {
     ecs_cluster_name = aws_ecs_cluster.ecs_cluster.name
