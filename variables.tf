4641189b39f661ffd9f3fbe2ad4d73c31985b911
Mark Honomichl <mark@austincloud.guru>
2019-11-11 10:30:20 -0600

    Added types

--- variables.tf
+++ variables.tf
@@ -13,31 +13,36 @@ variable "ecs_cluster_name" {
 
 variable "subnets" {
   description = "List of Subnet ID"
-  type        = list
+  type        = list(string)
 }
 
 variable "min_instance_size" {
   description = "Minimum number of EC2 instances."
+  type        = number
   default     = 1
 }
 
 variable "max_instance_size" {
   description = "Maximum number of EC2 instances."
+  type        = number
   default     = 1
 }
 
 variable "desired_instance_capacity" {
   description = "Desired number of EC2 instances."
+  type        = number
   default     = 1
 }
 
 variable "instance_type" {
   description = "Default instance type"
+  type        = string
   default     = "t3.medium"
 }
 
 variable "key_name" {
   description = "SSH key name in your AWS account for AWS instances."
+  type        = string
   default     = ""
 }
 
