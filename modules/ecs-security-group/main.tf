c085daffde6d57e6888cee6b40da5f2d128aebec
Mark Honomichl <mark@austincloud.guru>
2020-03-17 17:08:30 -0500

    Initial add of the security group module

--- modules/ecs-security-group/main.tf
+++ modules/ecs-security-group/main.tf
@@ -5,7 +5,6 @@ resource "aws_security_group_rule" "this" {
   type                     = "ingress"
   description              = var.description
   security_group_id        = var.security_group_id
-  source_security_group_id = var.source_security_group_id
   self                     = var.self
   from_port                = var.from_port
   protocol                 = var.protocol
