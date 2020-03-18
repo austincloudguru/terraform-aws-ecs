3c703d98b00d527cbc4411951c773360274bab2f
Mark Honomichl <mark@austincloud.guru>
2020-03-17 19:25:03 -0500

    Cleanup

--- modules/ecs-security-group/main.tf
+++ modules/ecs-security-group/main.tf
@@ -2,12 +2,12 @@
 # Create the Security Group Role
 #------------------------------------------------------------------------------
 resource "aws_security_group_rule" "this" {
-  type                     = "ingress"
-  description              = var.description
-  security_group_id        = var.security_group_id
-  self                     = var.self
-  from_port                = var.from_port
-  protocol                 = var.protocol
-  to_port                  = var.to_port
-  cidr_blocks              = var.cidr_blocks
+  type              = "ingress"
+  description       = var.description
+  security_group_id = var.security_group_id
+  self              = var.self
+  from_port         = var.from_port
+  protocol          = var.protocol
+  to_port           = var.to_port
+  cidr_blocks       = var.cidr_blocks
 }
