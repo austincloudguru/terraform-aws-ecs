0ec43dda6802ee090db0fd16a7aead27bed75e8a
Mark Honomichl <mark@honomichl.net>
2024-04-16 12:40:06 -0500

    Adding map public Ips

--- examples/terratest/main.tf
+++ examples/terratest/main.tf
@@ -25,14 +25,15 @@ data "aws_ami" "latest_ecs_ami" {
 }
 
 module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  name               = "terratest-vpc"
-  cidr               = "10.0.0.0/16"
-  azs                = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  source                  = "terraform-aws-modules/vpc/aws"
+  name                    = "terratest-vpc"
+  cidr                    = "10.0.0.0/16"
+  azs                     = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
+  map_public_ip_on_launch = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
 
   tags = {
     Owner       = "mark"
