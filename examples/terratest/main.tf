2e68b27c2b5207afdc7d2d5b0d24932cf61b1eb1
Mark Honomichl <mark@honomichl.net>
2024-04-16 13:42:50 -0500

    Updating the public IP

--- examples/terratest/main.tf
+++ examples/terratest/main.tf
@@ -25,15 +25,14 @@ data "aws_ami" "latest_ecs_ami" {
 }
 
 module "vpc" {
-  source                  = "terraform-aws-modules/vpc/aws"
-  name                    = "terratest-vpc"
-  cidr                    = "10.0.0.0/16"
-  azs                     = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
-  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
-  map_public_ip_on_launch = true
-  enable_nat_gateway      = true
-  single_nat_gateway      = true
+  source             = "terraform-aws-modules/vpc/aws"
+  name               = "terratest-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     Owner       = "mark"
@@ -42,16 +41,17 @@ module "vpc" {
 }
 
 module "ecs" {
-  source           = "../../modules/ecs"
-  name             = "terratest-cluster"
-  image_id         = data.aws_ami.latest_ecs_ami.image_id
-  vpc_id           = module.vpc.vpc_id
-  subnet_ids       = module.vpc.public_subnets
-  min_size         = "1"
-  max_size         = "1"
-  desired_capacity = "1"
-  instance_type    = "t3.large"
-  key_name         = "aws-main"
+  source                      = "../../modules/ecs"
+  name                        = "terratest-cluster"
+  image_id                    = data.aws_ami.latest_ecs_ami.image_id
+  vpc_id                      = module.vpc.vpc_id
+  subnet_ids                  = module.vpc.public_subnets
+  min_size                    = "1"
+  max_size                    = "1"
+  desired_capacity            = "1"
+  instance_type               = "t3.large"
+  key_name                    = "aws-main"
+  associate_public_ip_address = true
   tags = {
     Terraform = "true"
   }
